628a77237790af5acead49e8c0ab5e2b60ad4f7d
Wiam Nasr <wiamnasr@gmail.com>
2021-12-21 18:52:48 +0000

    filtering for term to be included in name surname or email as well as date (together if included or alone filtering)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const moment = require("moment");
+const validator = require("email-validator");
 
 const bodyParser = require("body-parser");
 
@@ -16,32 +17,96 @@ app.use(cors());
 const bookings = require("./bookings.json");
 
 app.get("/bookings/search", (req, res) => {
-  const { date } = req.query;
-  const formattedDate = moment(date).format("YYYY-MM-DD");
-  if (formattedDate !== date) {
-    return res.status(400).json({
-      success: false,
-      msg: "Please fix date search format as such: 'search?date=YYYY-MM-DD'",
+  const { date, term } = req.query;
+
+  if (date && term) {
+    const formattedDate = moment(date).format("YYYY-MM-DD");
+
+    const matchingBookings = bookings.filter(
+      (booking) =>
+        booking.email.toUpperCase().includes(term.toUpperCase()) ||
+        booking.firstName.toUpperCase().includes(term.toUpperCase()) ||
+        booking.surname.toUpperCase().includes(term.toUpperCase())
+    );
+
+    if (formattedDate !== date) {
+      return res.status(400).json({
+        success: false,
+        msg: "Please fix date search format as such: 'search?date=YYYY-MM-DD'",
+      });
+    } else if (matchingBookings.length === 0) {
+      return res.status(400).json({
+        success: false,
+        msg: "your search term did not return any matches",
+      });
+    } else {
+      const filteredBookings = bookings.filter(
+        (booking) =>
+          (booking.checkInDate === formattedDate ||
+            booking.checkOutDate === formattedDate) &&
+          (booking.email.toUpperCase().includes(term.toUpperCase()) ||
+            booking.firstName.toUpperCase().includes(term.toUpperCase()) ||
+            booking.surname.toUpperCase().includes(term.toUpperCase()))
+      );
+
+      if (filteredBookings.length === 0) {
+        return res.status(400).json({
+          success: false,
+          msg: "No bookings match your date and search term",
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        filteredBookings,
+      });
+    }
+  } else if (date) {
+    const formattedDate = moment(date).format("YYYY-MM-DD");
+    if (formattedDate !== date) {
+      return res.status(400).json({
+        success: false,
+        msg: "Please fix date search format as such: 'search?date=YYYY-MM-DD'",
+      });
+    }
+
+    const matchingBookingDate = bookings.filter(
+      (booking) =>
+        booking.checkInDate === formattedDate ||
+        booking.checkOutDate === formattedDate
+    );
+
+    if (matchingBookingDate.length === 0) {
+      return res.status(404).json({
+        success: false,
+        msg: "It appears no bookings match your search date",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      matchingBookingDate,
+    });
+  } else if (term) {
+    const matchingBookings = bookings.filter(
+      (booking) =>
+        booking.email.toUpperCase().includes(term.toUpperCase()) ||
+        booking.firstName.toUpperCase().includes(term.toUpperCase()) ||
+        booking.surname.toUpperCase().includes(term.toUpperCase())
+    );
+
+    if (matchingBookings.length === 0) {
+      return res.status(400).json({
+        success: false,
+        msg: "your search term did not return any matches",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      matchingBookings,
     });
   }
-
-  const matchingBookingDate = bookings.filter(
-    (booking) =>
-      booking.checkInDate === formattedDate ||
-      booking.checkOutDate === formattedDate
-  );
-
-  if (matchingBookingDate.length === 0) {
-    return res.status(404).json({
-      success: false,
-      msg: "It appears no bookings match your search date",
-    });
-  }
-
-  return res.status(200).json({
-    success: true,
-    matchingBookingDate,
-  });
 });
 
 app.delete("/bookings/:id", (req, res) => {
@@ -106,6 +171,21 @@ app.post("/bookings", (req, res) => {
   }
   const updatedBookings = [...bookings];
 
+  const emailValidator = validator.validate(email);
+  const checkStayRange = moment(checkOutDate) - moment(checkInDate);
+
+  if (!emailValidator) {
+    return res.status(404).json({
+      success: false,
+      msg: "please enter a valid email...",
+    });
+  } else if (checkStayRange < 0) {
+    return res.status(404).json({
+      success: false,
+      msg: "checkout date cannot be before checkin date...",
+    });
+  }
+
   updatedBookings.push({
     id: Math.random(),
     title,
@@ -116,7 +196,6 @@ app.post("/bookings", (req, res) => {
     checkInDate,
     checkOutDate,
   });
-  console.log(updatedBookings);
 
   return res.status(200).json({
     success: true,
